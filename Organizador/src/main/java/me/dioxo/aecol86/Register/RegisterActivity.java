7037f744b759b810f91bef588abe175e8a0c07a9
Dioxo <amolinares19@gmail.com>
2019-06-14 21:26:11 +0200

    Creation Register Activity

---
+++ Organizador/src/main/java/me/dioxo/aecol86/Register/RegisterActivity.java
@@ -0,0 +1,15 @@
+package me.dioxo.aecol86.Register;
+
+import androidx.appcompat.app.AppCompatActivity;
+import me.dioxo.aecol86.R;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
