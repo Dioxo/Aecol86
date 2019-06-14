712827dc2496a2b3333e08957ad9d3ed386abb86
Dioxo <amolinares19@gmail.com>
2019-06-14 20:02:38 +0200

    Migrate to AndroidX

--- Organizador/src/main/java/me/dioxo/aecol86/Authentication.java
+++ Organizador/src/main/java/me/dioxo/aecol86/Authentication.java
@@ -1,6 +1,6 @@
 package me.dioxo.aecol86;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Authentication extends AppCompatActivity {
