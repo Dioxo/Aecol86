b9368e95d6ccd78e31e28a24a61aa29be93e9495
Dioxo <amolinares19@gmail.com>
2019-06-14 21:40:34 +0200

    Request RegisterOrganizador

---
+++ Organizador/src/main/java/me/dioxo/aecol86/Request/RegisterOrganizador.java
@@ -0,0 +1,31 @@
+package me.dioxo.aecol86.Request;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+import me.dioxo.aecol86.Constantes;
+import me.dioxo.aecol86.Organizador;
+
+public class RegisterOrganizador extends StringRequest {
+    private static final String route = Constantes.REGISTER_USER;
+    private Map<String, String> parametres;
+
+    public RegisterOrganizador(Response.Listener<String> listener, Organizador organizador) {
+        super(Method.POST, route, listener, null);
+
+        /*
+         * Change params to send to request
+         * */
+        parametres = new HashMap<>();
+        parametres.put("email_organizador", organizador.getEmail());
+        parametres.put("nombre_organizador", organizador.getNombre());
+        parametres.put("password_organizador", organizador.getPassword());
+    }
+
+    @Override
+    protected Map<String, String> getParams() {
+        return parametres;
+    }
+}
