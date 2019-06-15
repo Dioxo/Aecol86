7346355a0029c42f366fd13d8f28d9cf90f3d8bf
Dioxo <amolinares19@gmail.com>
2019-06-15 13:34:29 +0200

    Creation InformationACtivity

---
+++ Organizador/src/main/java/me/dioxo/aecol86/Information/InformationActivity.java
@@ -0,0 +1,15 @@
+package me.dioxo.aecol86.Information;
+
+import androidx.appcompat.app.AppCompatActivity;
+import me.dioxo.aecol86.R;
+
+import android.os.Bundle;
+
+public class InformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_information);
+    }
+}
