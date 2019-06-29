f994c03ff1a66a9616ca372d1cb4d887585183e9
Dioxo <amolinares19@gmail.com>
2019-06-29 22:40:11 +0200

    MVP Autentication

--- Estudiante/src/main/java/me/dioxo/estudiante/Authentication/Authentication.java
+++ Estudiante/src/main/java/me/dioxo/estudiante/Authentication/Authentication.java
@@ -1,16 +1,103 @@
 package me.dioxo.estudiante.Authentication;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import me.dioxo.estudiante.R;
 
-public class Authentication extends AppCompatActivity {
+public class Authentication extends AppCompatActivity implements  Authentication_View {
+
+    @BindView(R.id.edTxtUser)
+    EditText edTxtUser;
+    @BindView(R.id.edTxtPassword)
+    EditText edTxtPassword;
+    @BindView(R.id.btnLogin)
+    Button btnLogin;
+    @BindView(R.id.progressBar)
+    ProgressBar progressBar;
+    private Authentication_Presenter presenter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentication);
+        ButterKnife.bind(this);
+
+        presenter = new Authentication_Presenter_Impl(this);
+        presenter.onCreate();
+        presenter.checkAlreadyConnected();
+        hideProgressBar();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        presenter.onDestroy();
+    }
+
+    public void enableInputs() {
+        edTxtPassword.setEnabled(true);
+        edTxtUser.setEnabled(true);
+        btnLogin.setEnabled(true);
+    }
+
+    public void disableInputs() {
+        edTxtPassword.setEnabled(false);
+        edTxtUser.setEnabled(false);
+        btnLogin.setEnabled(false);
+    }
+
+    public void goToNextPage() {
+        Toast.makeText(this, "YES", Toast.LENGTH_LONG).show();
+        /*Intent intent = new Intent(this, NavigationDrawer.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+        Log.i("Extras",edTxtUser.getText().toString());
+        intent.putExtra("email", edTxtUser.getText().toString());
+        startActivity(intent);*/
+    }
+
+    @Override
+    public void showProgressBar() {
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void hideProgressBar() {
+        progressBar.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void loginError(String error) {
+        effacerText();
+        edTxtPassword.setError(error);
+    }
+
+    private void effacerText() {
+        edTxtPassword.setText("");
+        edTxtUser.setText("");
+    }
+
+    @OnClick(R.id.btnLogin)
+    public void onBtnLoginClicked() {
+        presenter.confirmerMDP(edTxtUser.getText().toString(), edTxtPassword.getText().toString());
+        //effacerText();
+    }
+
+    @OnClick(R.id.register)
+    public void onRegisterClicked()
+    {
+        //startActivity(new Intent(this, RegisterActivity.class));
     }
 }
---
+++ Estudiante/src/main/java/me/dioxo/estudiante/Authentication/Authentication_Presenter_Impl.java
@@ -0,0 +1,87 @@
+package me.dioxo.estudiante.Authentication;
+
+
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import org.greenrobot.eventbus.Subscribe;
+
+import me.dioxo.aecol86.Constantes;
+import me.dioxo.estudiante.libs.ApplicationContextProvider;
+import me.dioxo.estudiante.libs.EventBus;
+import me.dioxo.estudiante.libs.GreenRobotEventBus;
+
+
+public class Authentication_Presenter_Impl implements Authentication_Presenter {
+    private Authentication_View view;
+    private EventBus eventBus;
+    private Authentication_Repository repository;
+
+    public Authentication_Presenter_Impl(Authentication_View view) {
+        this.view = view;
+        repository = new Authentication_Repository_Impl();
+        eventBus = GreenRobotEventBus.getInstance();
+    }
+
+    public void confirmerMDP(String email, String mdp) {
+        if(view != null){
+            view.showProgressBar();
+            view.disableInputs();
+        }
+
+        repository.confirmerMDP(email,mdp);
+    }
+
+    public void goToNextPage() {
+        view.goToNextPage();
+    }
+
+    public void onCreate() {
+        eventBus.register(this);
+    }
+
+    public void onDestroy() {
+        eventBus.unregister(this);
+    }
+
+    @Subscribe
+    public void onEventMainThread(Authentication_Event authenticationEvent) {
+        switch (authenticationEvent.getEventType()){
+
+            case Authentication_Event.AUTHENTICATION_OKAY:
+                if(view != null) {
+                    view.enableInputs();
+                    view.hideProgressBar();
+                    view.goToNextPage();
+                }
+                break;
+
+            case Authentication_Event.AUTHENTICATION_ERROR:
+                if(view != null){
+                    view.loginError(authenticationEvent.getMessage());
+                    view.enableInputs();
+                    view.hideProgressBar();
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void checkAlreadyConnected() {
+        SharedPreferences settings = ApplicationContextProvider.getContext().getSharedPreferences(Constantes.ID_USER, 0);
+        String id_user = settings.getString(Constantes.ID_USER,null);
+
+        if(id_user  != null) {
+            Log.i("Session LOl" , "Already Connected" );
+            Log.i("Session LOl" , id_user);
+
+            if (view != null) {
+                view.enableInputs();
+                view.hideProgressBar();
+                view.goToNextPage();
+            }
+        }
+
+    }
+
+}
