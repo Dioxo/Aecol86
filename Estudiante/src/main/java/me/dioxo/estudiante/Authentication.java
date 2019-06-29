3633abf72ab902b4fcb991d6484eb958a7aab4ca
Dioxo <amolinares19@gmail.com>
2019-06-29 20:59:58 +0200

    Creacion modulo Estudiante

---
+++ Estudiante/src/main/java/me/dioxo/estudiante/Authentication.java
@@ -0,0 +1,14 @@
+package me.dioxo.estudiante;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Authentication extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication);
+    }
+}
