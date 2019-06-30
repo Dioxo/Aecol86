206cbcee747d747fb7f88b7f33b0f8c60487b02b
Dioxo <amolinares19@gmail.com>
2019-07-01 01:18:53 +0200

    Constantes Estudiante

---
+++ Estudiante/src/main/java/me/dioxo/estudiante/Constantes.java
@@ -0,0 +1,16 @@
+package me.dioxo.estudiante;
+
+public class Constantes {
+    private static final String SERVER_ROUTE = "http://138.68.69.52/aecol86/";
+    //private static final String SERVER_ROUTE = "http://192.168.43.7/aecol86/";
+    public static final String LOGIN_ROUTE_ESTUDIANTE = SERVER_ROUTE+"LoginEstudiante.php";
+    public static final String REGISTER_USER =  SERVER_ROUTE + "RegisterOrganizador.php";
+    public static final String CHERCHER_PERSONAS = SERVER_ROUTE + "ChercherPersonas.php";
+    public static final String DELETE_PERSONA =  SERVER_ROUTE + "DeletePersona.php";
+    public static final String INSERT_PERSONA =  SERVER_ROUTE + "InsertPersona.php";
+    public static final String UPDATE_PERSONA =  SERVER_ROUTE + "UpdatePersona.php";
+    public static final String CHERCHER_ORGANIZADORES = SERVER_ROUTE + "ChercherOrganizadores.php";
+
+
+    public static  String ID_USER = "id_user";
+}
