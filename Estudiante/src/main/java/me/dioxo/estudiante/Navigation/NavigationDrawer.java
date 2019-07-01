62ae545d887caec09c7b48c1533bee143340f5bc
Dioxo <amolinares19@gmail.com>
2019-07-02 00:15:24 +0200

    Transiciones de Fragment

--- Estudiante/src/main/java/me/dioxo/estudiante/Navigation/NavigationDrawer.java
+++ Estudiante/src/main/java/me/dioxo/estudiante/Navigation/NavigationDrawer.java
@@ -1,33 +1,45 @@
 package me.dioxo.estudiante.Navigation;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import android.view.View;
-
-import androidx.core.view.GravityCompat;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
-import com.google.android.material.navigation.NavigationView;
-
-import androidx.drawerlayout.widget.DrawerLayout;
-
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
-import android.view.Menu;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import me.dioxo.estudiante.Authentication.Authentication;
+import me.dioxo.estudiante.Constantes;
+import me.dioxo.estudiante.MyInfo.MyInformationFragment;
+import me.dioxo.estudiante.R;
+import me.dioxo.estudiante.libs.ApplicationContextProvider;
 
 public class NavigationDrawer extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    @BindView(R.id.fab)
+    FloatingActionButton fab;
+
+    TextView email;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation_drawer);
+        ButterKnife.bind(this);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
@@ -45,6 +57,26 @@ public class NavigationDrawer extends AppCompatActivity
         drawer.addDrawerListener(toggle);
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
+
+
+        View headerView = navigationView.getHeaderView(0);
+        email = (TextView) headerView.findViewById(R.id.txtEmailUser);
+        Intent intent = getIntent();
+
+
+        String emailOrganizador = intent.getStringExtra("email");
+        email.setText(emailOrganizador);
+
+        cargarFragmentPorDefecto();
+    }
+
+    private void cargarFragmentPorDefecto() {
+        Fragment fragment = new MyInformationFragment();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.screen_area, fragment)
+                .commit();
+
+        getSupportActionBar().setTitle("Mis datos");
     }
 
     @Override
@@ -72,12 +104,26 @@ public class NavigationDrawer extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.menu_logout) {
+            cerrarSesion();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void cerrarSesion() {
+        SharedPreferences settings = ApplicationContextProvider.getContext().getSharedPreferences(Constantes.ID_USER, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString(Constantes.ID_USER, null);
+        // Commit the edits!
+        editor.apply();
+
+        Intent intent = new Intent(this, Authentication.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
@@ -85,18 +131,34 @@ public class NavigationDrawer extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_home) {
+        boolean transition = false;
+        Fragment fragment = null;
+
+        if (id == R.id.myInfo) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+            transition = true;
+            fragment = new MyInformationFragment();
+        }/* else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
 
-        } else if (id == R.id.nav_tools) {
+        } else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
 
+        }*/
+
+        if (transition) {
+
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.screen_area, fragment)
+                    .commit();
+
+            item.setChecked(true);
+
+            getSupportActionBar().setTitle(item.getTitle());
         }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
